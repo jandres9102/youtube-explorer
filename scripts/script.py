a76e0cee201b643490f8fcc5b221e88b81daadab
jandres9102 <jandres9102@gmail.com>
2022-02-01 17:31:07 +0100

    Update script.py
    adding exception to the code

--- scripts/script.py
+++ scripts/script.py
@@ -8,12 +8,16 @@ def main():
     id_list = get_id()
     base_url = "https://www.youtube.com/watch?v="
     ydl_opts = {}
-    for elt in id_list : # loop through all id 
-        working_url = base_url + elt # create a link with base_url and the id 
-        # downloading meta data for the video
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl : 
-            meta = ydl.extract_info(working_url,download=False)
-        # put the meta data 
+    failed_videos=[]
+    for elt in id_list : # loop through all id
+        try:
+            working_url = base_url + elt # create a link with base_url and the id 
+            # downloading meta data for the video
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl : 
+                meta = ydl.extract_info(working_url,download=False)
+            # put the meta data 
+        except Exception as err:
+            failed_videos.append({'id':elt,'error':str(err)})
     
     return 0
 
