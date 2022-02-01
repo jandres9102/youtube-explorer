70b25c65ec48650500233839fcaf91f9932f1f0c
rjallat2 <93271515+rjallat2@users.noreply.github.com>
2022-02-01 14:26:49 +0100

    Update __main__.py

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -3,12 +3,13 @@ import os
 import sys
 
 
-#id = 'L-CYdL7NksA' 
+#liste des id des vidéos 
 id_list = [
         'L-CYdL7NksA',
         'eiLo-Xqrc_w'
 ]
 
+# nom de notre fichier json qui récupere les infos
 test = 'test'
 for i in id_list : 
     commande = "youtube-comment-downloader --youtubeid " + i + " --output " + test +".json"
