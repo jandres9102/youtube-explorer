9ba3aad9bb16835137edce583e945268184d69ff
Mysterion186 <kowsikan@hotmail.fr>
2022-02-01 15:40:11 +0100

    code to get video comment

--- scripts/comment.py
+++ scripts/comment.py
@@ -1,2 +1,19 @@
 import os, json 
+from database import *
 
+def main():
+    id_list = get_id()
+    for elt in id_list:
+        # code to create a txt file with all the comments
+        commande = "youtube-comment-downloader --youtubeid " +elt+ " --output comments.txt"
+        os.system(commande)
+
+        # readin the txt file
+        with open("comments.txt","r") as f:
+            a = f.readlines()
+        for line in a : 
+            # convert the string to a json 
+            temp = json.loads(line)
+            # gather only the required information (comment id, the comment and the votes)
+            temp_dict = {"comment_id" : temp["cid"],"texte" : temp["text"],"votes" : temp["votes"]}
+            # code to insert this json to the mongodatabase
