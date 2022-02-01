e36c2c8ad7b0075301eead50650408564d2ea15b
Mysterion186 <kowsikan@hotmail.fr>
2022-02-01 15:40:59 +0100

    renamed the meta analysis script

--- scripts/script.py
+++ scripts/script.py
@@ -1,17 +1,7 @@
 from __future__ import unicode_literals
-import youtube_dl 
-from pymongo import MongoClient # import to communicate with de Mongo database 
-
-# function to connect to the database
-def connect(login,password):
-    pass 
-
-# function to retrieve video's id 
-def get_id():
-    pass 
-# function to store date in the Mongo database 
-def store_data():
-    pass 
+from database import *
+import os 
+import json 
 
 # main function to download all data 
 def main(): 
