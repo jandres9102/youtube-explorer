756ac232075394277dc643871782d6395e20d0a5
Mysterion186 <kowsikan@hotmail.fr>
2022-02-01 11:37:54 +0100

    code to get several meta information

---
+++ download_test.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+import youtube_dl 
+
+ydl_opts = {}
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl : 
+    meta = ydl.extract_info('https://www.youtube.com/watch?v=9bZkp7q19f0',download=False)
+
+for key in meta.keys():
+    print(f"pour la clé {key} oa l'information {meta[key]}")
