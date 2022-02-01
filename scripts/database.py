44881388e806d241d431ce7255a0a70cca007b95
Mysterion186 <kowsikan@hotmail.fr>
2022-02-01 15:30:25 +0100

    add commun database function

---
+++ scripts/database.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient # import to communicate with de Mongo database 
+
+
+# function to connect to the database
+def connect(login,password):
+    pass 
+
+# function to retrieve video's id 
+def get_id():
+    pass 
+# function to store date in the Mongo database 
+def store_data():
+    pass 
