8bc09f174f11a57c0037d7230235e488b07d31b8
Mysterion186 <kowsikan@hotmail.fr>
2022-02-01 15:43:25 +0100

    version sans mongo

--- scripts/comment.py
+++ scripts/comment.py
@@ -3,6 +3,7 @@ from database import *
 
 def main():
     id_list = get_id()
+    result_list = [] # list to see the result
     for elt in id_list:
         # code to create a txt file with all the comments
         commande = "youtube-comment-downloader --youtubeid " +elt+ " --output comments.txt"
@@ -17,3 +18,8 @@ def main():
             # gather only the required information (comment id, the comment and the votes)
             temp_dict = {"comment_id" : temp["cid"],"texte" : temp["text"],"votes" : temp["votes"]}
             # code to insert this json to the mongodatabase
+            result_list.append(temp_dict) # temporary 
+    return result_list
+
+if __name__=="__main__": 
+    main()
