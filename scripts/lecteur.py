591b58f6cbb35a88ae02e5b79301279bd079c34d
Haïtham Souef <haitham.souef@gmail.com>
2022-02-02 19:58:30 +0100

    put ids in mongoDb

---
+++ scripts/lecteur.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from database import *
+
+
+
+db,col = connect('db','id')
+
+csv_read = pd.read_csv('2021_10_08_video_ids.csv', sep =';')
+
+
+
+for idx in csv_read['video_id '][:10] :
+     store_data(col,{"id":idx})
+
+
+print(list(col.find()))
+
+# col.drop()
