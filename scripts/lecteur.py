08321ba1773a03065ebd76c78213245402e473cb
Mysterion186 <kowsikan@hotmail.fr>
2022-02-08 10:50:21 +0100

    télécharger le fichier avec les id directement en csv

--- scripts/lecteur.py
+++ scripts/lecteur.py
@@ -2,14 +2,13 @@ import pandas as pd
 from database import *
 
 
-
 db,col = connect('db','id')
 
 csv_read = pd.read_csv('2021_10_08_video_ids.csv', sep =';')
 
 
 
-for idx in csv_read['video_id '][:10] :
+for idx in csv_read['video_id'][:10] :
      store_data(col,{"id":idx})
 
 
