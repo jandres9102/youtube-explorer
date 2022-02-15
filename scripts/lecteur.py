bdb7609b9692431f31069c64239e4ede943aad36
Mysterion186 <kowsikan@hotmail.fr>
2022-02-15 11:14:50 +0100

    updated csv file path

--- scripts/lecteur.py
+++ scripts/lecteur.py
@@ -2,7 +2,6 @@ import pandas as pd
 from database import *
 from pathlib import Path
 
-print("Avant le connect ")
 db,col = connect('db','id')
 #os.path.abspath('2021_10_08_video_ids.csv')
 csv_read = pd.read_csv(Path('scripts/2021_10_08_video_ids.csv').absolute(), sep =';')
@@ -14,5 +13,4 @@ for idx in csv_read['video_id'][:10] :
 
 
 print(list(col.find()))
-print("On est après le connect")
 # col.drop()
