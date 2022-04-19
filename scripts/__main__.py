c936aa2799687c369b19bd1b638880fd28105d9b
Andres Villarraga <jandres9102@gmail.com>
2022-04-19 19:37:00 +0200

    implementation of age-gender recognition1

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -1,14 +1,13 @@
 import comment as c # import function to download comments
 import script as sc # import function to download meta-data
+from age_gender_predictor import AgeGenderPredictor
 from multiprocessing import Process # import to parallize comment and video download 
 
 def main(file_value = 10):
 	c.main(file_value) #function to  download comment
 	sc.main() #function to download meta-data 
 
-if __name__ == "__main__":
-
-	# main()
+if __name__ == "__main__" :
 	processor = [] # list that will contain all of our 8 process 
 	for k in range(1,9):
 		if k%2==0 : 
@@ -17,7 +16,13 @@ if __name__ == "__main__":
 			p = Process(target=c.main,args=(k,))   
 		processor.append(p) # add this process to the list 
 		p.start() # start of the process 
-	
 	for elt in processor:
 		elt.join() # to end all processes
+	#location of the NN models related to age-gender recognition
+	genderModel=['models/gender','gender.caffemodel','gender.prototxt']
+	ageModel=['models/age','dex_chalearn_iccv2015.caffemodel','age.prototxt']
+	#declaring an object to charge these models
+	aGenPred= AgeGenderPredictor(genderModel,ageModel)
+	#the input parameter is where the images to apply the NN are located
+	print(aGenPred.get_people_age_gender('./images/'))
 	
